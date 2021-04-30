000b65b4050c24948e897f7552686f3caa263dd9
Alberto Rosas <alberto.rsesc@protonmail.com>
2021-04-30 15:51:04 -0700

    Google verification meta tag added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="google-site-verification" content="SCtMxv5igpA_geVlWon_J8b0PmgBTkb81jATbcpYYS8" />
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏡</text></svg>">
         @yield('meta')
 
