6b138eaac93a41c991eb42840249af3f6b06ed78
Alberto Rosas <alberto.rsesc@protonmail.com>
2021-04-30 22:01:54 -0700

    Minor fixes

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,7 +2,7 @@
 
 window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-axios.defaults.baseURL = 'https://app.houseify.io/api/'
+window.axios.defaults.baseURL = process.env.MIX_APP_URL
 window.axios.defaults.withCredentials = true;
 
 let token = document.head.querySelector('meta[name="csrf-token"]');
