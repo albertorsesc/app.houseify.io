13b2d76a1f2f29349e321a67ad983490194f999c
Alberto Rosas <alberto.rsesc@protonmail.com>
2021-05-17 22:21:50 -0700

    Roadmap added

--- resources/js/components/Properties/PropertyImages.vue
+++ resources/js/components/Properties/PropertyImages.vue
@@ -25,6 +25,7 @@
                         <template #content>
                             <div class="md:flex md:flex-wrap">
                                 <div class="w-full items-center md:w-32 p-2" v-for="image in images" :key="image.id">
+                                    <!--baseUrl + `/img/small/${image.file_name.split('public/').pop()}`-->
                                     <img :src="image.file_name"
                                          class="h-32 w-32 z-0"
                                          title="Eliminar imágen"
