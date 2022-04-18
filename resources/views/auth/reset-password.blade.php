26c2245b1b02a690c08813798c6c24b60024652e
Alberto Rosas <alberto.rsesc@protonmail.com>
2022-04-18 00:01:09 -0700

    Cleaning views

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -4,12 +4,6 @@
             <x-jet-authentication-card-logo />
         </x-slot>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
-
         <x-jet-validation-errors class="mb-4" />
 
         <form method="POST" action="{{ route('password.update') }}">
